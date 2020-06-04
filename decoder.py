c9c2231447c46b4067781008242b18f070eca1e6
Sergey <mackovckin.sergei@yandex.ru>
2020-06-05 01:29:43 +0300

    delete/move/alter/add

--- decoder.py
+++ decoder.py
@@ -33,76 +33,115 @@ MONTH_DICT = {
 
 class TextDecoder():
 
-    def __init__(self, text):
-        self.text = text.lower()
+    def __init__(self):
+        self.now = datetime.datetime.now()
 
-    def check_text(self):
-        print(self.text)
-        if self.text.startswith('список'):
-            list_date_expression = re.compile(r'список ((?P<today>сегодня)|(?P<tomorrow>завтра)|(?P<day>\b\d{1,2}\b)\s?(?P<month>\w+))')
-            date_match = re.search(list_date_expression, self.text)
-            return('list')
-
-        else:
-            test_expression = re.compile(r'(((?P<today>сегодня)|(?P<tomorrow>завтра)|(?P<day>\b\d{1,2}\b)\s?(?P<month>\w+))\s)?(в\s)?(?P<hour>\d{1,4})(?P<minutes>:\d{2})?\s(?P<task>.*)')
-            test_match = re.search(test_expression, self.text)
-
-            if test_match:
-                today = test_match.group('today')
-                tomorrow = test_match.group('tomorrow')
-                day = test_match.group('day')
-                month = test_match.group('month')
-                hour = test_match.group('hour')
-                minutes = test_match.group('minutes')
-                task = test_match.group('task')
-
-                if tomorrow:
-                    add_day = datetime.datetime.now().day + 1
-                    add_month = datetime.datetime.now().month
-                    add_year = datetime.datetime.now().year
-                
-                elif day:
-                    add_day = int(day)
-                    add_year = datetime.datetime.now().year
-                    if month in MONTH_DICT.keys():
-                        add_month = MONTH_DICT[month]
-
-
+    def date_parser(self, text):
+        date_expression = re.compile(r'((?P<today>сегодня)|(?P<tomorrow>завтра)|(?P<day>\b\d{1,2}\b)\s?(?P<month>\w+))')
+        date_search = re.search(date_expression, text)
+        if date_search:
+            if date_search.group('today'):
+                return_date = datetime.datetime(self.now.year, self.now.month, self.now.day)
+            elif date_search.group('tomorrow'):
+                return_date = datetime.datetime(self.now.year, self.now.month, self.now.day+1)
+            elif date_search.group('day') and date_search.group('month'):
+                month = MONTH_DICT.get(date_search.group('month'))
+                if month:
+                    return_date = datetime.datetime(self.now.year, int(month), int(date_search.group('day')))
                 else:
-                    add_day = datetime.datetime.now().day
-                    add_month = datetime.datetime.now().month
-                    add_year = datetime.datetime.now().year
+                    return_date = None
 
-                if hour:
-                    if not minutes:
-                        if len(hour) < 3:
-                            add_minutes = 0
-                            add_hour = int(hour)
-                        elif len(hour) == 3:
-                            add_hour = int(hour[:1])
-                            add_minutes = int(hour[1:])
-                        elif len(hour) == 4:
-                            add_hour = int(hour[:2])
-                            add_minutes = int(hour[2:])
-                    else:
-                        add_hour = int(hour)
-                        add_minutes = int(minutes[1:])
+            if return_date:
+                return(re.sub(date_search.group(), '', text).strip(), return_date)
 
 
-                
+    def time_parser(self, text):
+        time_expression = re.compile(r'((в\s)?((?P<time_with_sep>\d{1,2}:\d{2})|(?P<time_without_sep>\d{1,4}\s)))')
+        time_search = re.search(time_expression, text)
 
-                if not all([add_day, add_month, add_year, task]) and add_hour != None:
-                    print('Something is missing')
-
-                else:
-                    try:
-                        return_date = datetime.datetime(add_year, add_month, add_day, add_hour, add_minutes)
-                        return_task = task
-                        print(return_date)
-                        return((return_date, return_task))
-                    except Exception as e:
-                        print(e)
+        if time_search:
             
+            time_with_sep = time_search.group('time_with_sep')
+            time_without_sep = time_search.group('time_without_sep')
+
+            if time_with_sep:
+                hour, minutes = time_with_sep.split(':')
+                return_time = datetime.timedelta(hours = int(hour), minutes = int(minutes))
+
+            elif time_without_sep:
+                if len(time_without_sep) < 3:
+                    return_time = datetime.timedelta(hours = int(time_without_sep))
+                elif len(time_without_sep) == 3:
+                    return_time = datetime.timedelta(hours = int(time_without_sep[0]), minutes = int(time_without_sep[1:3]))
+                else:
+                    return_time = datetime.timedelta(hours = int(time_without_sep[0:2]), minutes = int(time_without_sep[2:4]))
+            
+            return(re.sub(time_search.group(), '', text).strip(), return_time)
+
+
+
+    def datetime_parse(self, text):
+        date_data = self.date_parser(text)
+        if date_data:
+            parsed_text, date = date_data
+            time_data = self.time_parser(parsed_text)
+            if time_data:
+                task, time = time_data
+                final_time = date + time
+                return((final_time, task))
             else:
-                print('Wrong input')
+                final_time = date + datetime.timedelta(hours = 12)
+                task = parsed_text
+                return((final_time, task))
+        else:
+            time_data = self.time_parser(text)
+            if time_data:
+                task, time = time_data
+                final_time = datetime.datetime(self.now.year, self.now.month, self.now.day) + time
+                return((final_time, task))    
+
+
+
+    def main_parser(self, text):
+        prefix_expression = re.compile(r'(((?P<delete>^удалить)|(?P<alter>^изменить)|(?P<move>^перенести))\s(?P<key>\d+)\s?)|(?P<list>^список)')
+        prefix_search = re.search(prefix_expression, text)
+
+        if prefix_search:
+            key = prefix_search.group('key')
+            if prefix_search.group('list'):
+                string_to_parse = re.sub('список', '', text)
+                list_date = self.date_parser(string_to_parse)
+                if list_date:
+                    period_start = list_date[1]
+                    period_end = (list_date[1] + datetime.timedelta(days = 1))
+                    return({'type':'list', 'period': [int(period_start.timestamp()), int(period_end.timestamp())]})
+                else:
+                    return({'type':'list', 'period': list_date})
+            if key:
+                if prefix_search.group('delete'):
+                    return({'type' : 'remove', 'key' : key})
+                elif prefix_search.group('alter'):
+                    new_task = re.sub(prefix_search.group(), '', text)
+                    return({'type' : 'alter', 'key' : key, 'task' : new_task})
+                elif prefix_search.group('move'):
+                    string_to_parse = re.sub(prefix_search.group(), '', text)
+                    if self.datetime_parse(string_to_parse):
+                        new_datetime = self.datetime_parse(string_to_parse)[0]
+                        return({'type' : 'move', 'key' : key, 'time' : int(new_datetime.timestamp())})
+        
+        else:
+            if self.datetime_parse(text):
+                time, task = self.datetime_parse(text)
+                return({'type' : 'add', 'task' : task, 'time' : time})
+
+
+if __name__ == '__main__':
+
+    test_text = ['Напомни 2330 приготовить', 'сегодня в 2:00 meeting', '27 ноября 11:00 собрание', 'завтра в 01:00 meeting',
+                '18 июня в 10:00 meeting and other stuff', '18:00 and other stuff', 'Something else', '18 ноября собрание',
+                'сегодня в 200 sleepy sleep', 'удалить 12', 'изменить 18 new meeting', 'перенести 44 27 ноября 11:00', 'перенести 5 1:00',
+                'перенести 44 сегодня 11:00', 'перенести 5 завтра 1:00', 'список', 'список сегодня', 'список завтра', 'список 27 ноября']
+
+    for command in test_text:
+        print(TextDecoder().main_parser(command))
 
--- websocket.py
+++ websocket.py
@@ -5,13 +5,20 @@ import os
 import json
 import aiohttp
 import random
-import datetime
+from datetime import datetime
 import functools
 import speech_recognition as sr
 from threading import Thread
 import time
 from decoder import TextDecoder
 import re
+
+DEBUG  = (os.environ.get('DEBUG') == 'True')
+
+if DEBUG:
+    REDIS_TOKEN = 'redis://localhost:6379'
+else:
+    REDIS_TOKEN = os.environ.get('REDIS_TOKEN')
 
 
 class SpeechRecognitionThread(Thread):
@@ -50,12 +57,66 @@ class AsyncTelegramListener():
         self.SEND_MESSAGE_URL = os.path.join(self.URL, 'sendMessage')
         self.MESSAGE_DELAY = 300 ### 5 minutes
         self.SEND_MESSAGE_INTERVAL = 120 ### 2 minutes before and after
-        self.REDIS_TOKEN = os.environ.get('REDIS_TOKEN')
+        self.REDIS_TOKEN = REDIS_TOKEN
+
+    async def redis_connection_listener(self, request_interval):
+
+        redis = await aioredis.create_redis_pool(self.REDIS_TOKEN, encoding="utf-8")
+        while True:
+            users = await redis.smembers('users')
+            for user in users:
+                first_task = await redis.zrange(user, 0, 0)
+                all_tasks = await redis.zrange(user, 0, -1)
+                print(all_tasks)
+                if first_task:
+                    try:
+                        time, task = first_task[0].split('.')
+                        time_delta =  int(time) - int(datetime.now().timestamp())
+                        if abs(time_delta) < self.SEND_MESSAGE_INTERVAL:
+                            print('Send message to user', user)
+                            message_to_send = f'You asked us to remind you about: {task}'
+                            await redis.zrem(user, first_task[0])
+                            try:
+                                async with self.session.post(self.SEND_MESSAGE_URL, data= {'chat_id': user, 'text': message_to_send}) as response:
+                                    print(response.status)
+                                    if response.status == 200:
+                                        print('Success')
+                                    else:
+                                        print(f'Message for user {user} was delayed')
+                                        delay = int(time) + self.MESSAGE_DELAY
+                                        await redis.zadd(user, delay, task)
+                            except Exception as e:
+                                print(e)
+                        elif time_delta < -200:
+                            await redis.zrem(user, first_task[0])
+                            print('Delte unsend message. Time expired')
+                        else:
+                            print('Idiling... ')
+                    except Exception as e:
+                        print(e)
+
+            await asyncio.sleep(request_interval)
+
+        await redis.wait_closed()
+
+
+
+
+class AsyncWebsocketListener():
+
+    def __init__(self, test_queue):
+        self.session = aiohttp.ClientSession()
+        self.TOKEN = os.environ.get('REMINDME_TOKEN')
+        self.URL = f'https://api.telegram.org/bot{self.TOKEN}'
+        self.SEND_MESSAGE_URL = os.path.join(self.URL, 'sendMessage')
+        self.MESSAGE_DELAY = 300 ### 5 minutes
+        self.SEND_MESSAGE_INTERVAL = 120 ### 2 minutes before and after
+        self.REDIS_TOKEN = REDIS_TOKEN
         self.voice_queue = asyncio.Queue()
         self.message_queue = asyncio.Queue()
         self.decode_queue = asyncio.Queue()
+        self.test_queue = test_queue
         self.start_converters()
-    
 
     def start_converters(self):
         for i in range(5):
@@ -63,6 +124,7 @@ class AsyncTelegramListener():
             task = loop.create_task(self.voice_converter(self.voice_queue))
             decode_task = loop.create_task(self.text_decoder(self.decode_queue))
             message_task = loop.create_task(self.message_sender(self.message_queue))
+            test_task = loop.create_task(self.test_decoder(self.test_queue))
 
     async def message_sender(self, queue):
         while True:
@@ -78,7 +140,7 @@ class AsyncTelegramListener():
     async def text_decoder(self, queue):
         while True:
             message, user_id = await queue.get()
-            decoder = TextDecoder(message).check_text()
+            decoder = TextDecoder().main_parser(message)
             if decoder == 'list':
                 print('User is asking for list of tasks')
                 await self.redis_list_returner(user_id)
@@ -91,51 +153,100 @@ class AsyncTelegramListener():
                 
             else:
                 await self.message_queue.put(('We were not able to create task from your request', user_id))
-    
-    async def redis_connection_listener(self, request_interval):
 
-        redis = await aioredis.create_redis_pool(self.REDIS_TOKEN, encoding="utf-8")
+    async def test_decoder(self, queue):
         while True:
-            print('Connecting to Redis database')
-            users = await redis.smembers('users')
-            for user in users:
-                first_task = await redis.zrange(user, 0, 0)
-                print(first_task)
-                if first_task:
-                    time, task = first_task[0].split('.')
-                    time_delta =  int(time) - int(datetime.datetime.now().timestamp())
-                    if abs(time_delta) < self.SEND_MESSAGE_INTERVAL:
-                        print('Send message to user', user)
-                        message_to_send = f'You asked us to remind you about: {task}'
-                        await redis.zrem(user, first_task[0])
-                        try:
-                            async with self.session.post(self.SEND_MESSAGE_URL, data= {'chat_id': user, 'text': message_to_send}) as response:
-                                print(response.status)
-                                if response.status == 200:
-                                    print('Success')
-                                else:
-                                    print(f'Message for user {user} was delayed')
-                                    delay = int(time) + self.MESSAGE_DELAY
-                                    await redis.zadd(user, delay, task)
-                        except Exception as e:
-                            print(e)
-                    elif time_delta < -200:
-                        await redis.zrem(user, first_task[0])
-                        print('Delte unsend message. Time expired')
+            message = await queue.get()
+            test_id = '540863534'
+            test_id_liza = '396538767'
+            decoder = TextDecoder().main_parser(message)
+            print(decoder)
+            if decoder:
+                redis = await aioredis.create_redis_pool(self.REDIS_TOKEN, encoding="utf-8")
+                number_of_tasks = await redis.zcard(test_id)
+                command_type = decoder.get('type')
+                if command_type == 'add':
+                    await redis.zadd(test_id, int(decoder.get('time').timestamp()), str(int(decoder.get('time').timestamp()))+'.'+decoder.get('task'))
+                    tasks_list = await redis.zrange(test_id , 0, -1)
+                    show_time = decoder.get('time').strftime('%H:%M %d.%m.%y')
+                    await self.message_queue.put((f'Reminder has been set to {show_time}', test_id))
+
+                elif command_type == 'list':
+                    if number_of_tasks == 0:
+                        await self.message_queue.put((f'No current tasks waiting to be completed', test_id))
                     else:
-                        print('Idiling... ')
+                        period = decoder.get('period') 
+                        if period:
+                            return_list = await redis.zrangebyscore(test_id, period[0], period[1])
+                            if return_list:
+                                start_index = await redis.zrank(test_id, return_list[0])
+                                list_title = datetime.fromtimestamp(int(period[0])).strftime('%d.%m.%y')
+                            else:
+                                await self.message_queue.put((f'No tasks for specified date', test_id))
+                        else:
+                            return_list = await redis.zrange(test_id, 0, -1)
+                            start_index = 0
+                            list_title = 'Full list'
 
-            await asyncio.sleep(request_interval)
+                        reply_meassge = list_title + '\n'  
+                        for index, entry in enumerate(return_list):
+                            task = entry.split('.')[1]
+                            if period:
+                                time = datetime.fromtimestamp(int(entry.split('.')[0])).strftime('%H:%M')
+                            else:
+                                time = datetime.fromtimestamp(int(entry.split('.')[0])).strftime('%d.%m.%y %H:%M')
+                            reply_meassge += str(index+1)+') ' + time + ' ' + task.capitalize() + ' {'+str(index+start_index+1)+'}\n'
+                        await self.message_queue.put((f'{reply_meassge}', test_id))
+                
+                elif command_type == 'alter':
+                    key = int(decoder.get('key'))
+                    if key <= number_of_tasks:
+                        await asyncio.sleep(1)
+                        new_task = decoder.get('task')
+                        old_task = await redis.zrange(test_id, key, key)
+                        remaining_timestamp = old_task[0].split('.')[0]
+                        old_task_text = old_task[0].split('.')[1]
+                        await redis.zrem(test_id, old_task[0])
+                        await redis.zadd(test_id, int(remaining_timestamp), f'{remaining_timestamp}.{new_task}')
+                        number_of_tasks = await redis.zcard(test_id)
+                        updated_task = await redis.zrange(test_id, key, key)
+                        await self.message_queue.put((f'Task has been updated from "{old_task_text}" to "{new_task}"', test_id))
+                    else:
+                        await self.message_queue.put((f'Task with key {key} doesn\'t exist', test_id))
 
-        redis.close()
-        await redis.wait_closed()
+                elif command_type == 'remove':
+                    key = int(decoder.get('key')) - 1
+                    if key <= number_of_tasks:
+                        task_to_delete = await redis.zrange(test_id, key, key)
+                        task_text = task_to_delete[0].split('.')[1]
+                        await redis.zrem(test_id, task_to_delete[0])
+                        await self.message_queue.put((f'Task {task_text} has been removed!', test_id))
+                    else:
+                        await self.message_queue.put((f'Task with key {key} doesn\'t exist', test_id))
+
+                elif command_type == 'move':
+                    key = int(decoder.get('key')) - 1
+                    if key <= number_of_tasks:
+                        task_to_move = await redis.zrange(test_id, key, key)
+                        task_text = task_to_move[0].split('.')[1]
+                        time_to_message = datetime.fromtimestamp(decoder.get('time')).strftime('%d.%m.%y %H:%M')
+                        new_time = decoder.get('time')
+                        await redis.zrem(test_id, task_to_move[0])
+                        await redis.zadd(test_id, decoder.get('time'), f'{new_time}.{task_text}')
+                        await self.message_queue.put((f'Task "{task_text}" has been moved to {time_to_message}', test_id))
+                    else:
+                        await self.message_queue.put((f'Task with key {key} doesn\'t exist', test_id))
+
+                await asyncio.sleep(1)    
+                queue.task_done()
+                await redis.wait_closed()
 
     async def redis_manager(self, user_id, time, task):
 
         redis = await aioredis.create_redis_pool(self.REDIS_TOKEN)
         await redis.sadd('users', user_id)
         await redis.zadd(str(user_id), int(time), f'{int(time)}.{task}')
-        redis.close()
+        #redis.close()
         await redis.wait_closed()
 
     
@@ -145,15 +256,15 @@ class AsyncTelegramListener():
         task_list = await redis.zrange(user_id, 0, -1)
         if task_list:
             task_list = [(task.split('.')) for task in task_list]
-            return_task_list = [(datetime.datetime.fromtimestamp(int(task[0])).strftime('%d.%m.%y %H:%M'), task[1]) for task in task_list]
+            return_task_list = [(datetime.fromtimestamp(int(task[0])).strftime('%d.%m.%y %H:%M'), task[1]) for task in task_list]
             reply_message = ''
             for index, task in enumerate(task_list):
-                task_time = datetime.datetime.fromtimestamp(int(task[0])).strftime('%d.%m.%y %H:%M')
+                task_time = datetime.fromtimestamp(int(task[0])).strftime('%d.%m.%y %H:%M')
                 reply_message += f'{index+1}) {task_time} ----> {task[1]}\n'
             await self.message_queue.put((reply_message, user_id))
         else:
             await self.message_queue.put(('В списке заданий покати шаром', user_id))
-        redis.close()
+        #redis.close()
         await redis.wait_closed()
 
 
@@ -171,8 +282,7 @@ class AsyncTelegramListener():
     async def voice_converter(self, voice_queue):
         while True:
             download_link, user_id = await voice_queue.get()
-            #print(f'Started download link {download_link} {datetime.datetime.now()}')
-            time = datetime.datetime.now().strftime('%d_%m_%y_%H_%M_%S')
+            time = datetime.now().strftime('%d_%m_%y_%H_%M_%S')
             proc = await asyncio.create_subprocess_shell(
                 f'ffmpeg -i {download_link} -f wav -',
                 stdout=asyncio.subprocess.PIPE
@@ -181,12 +291,17 @@ class AsyncTelegramListener():
             thread = SpeechRecognitionThread(stdout, self.message_queue, self.decode_queue, user_id)
             thread.start()
             voice_queue.task_done()
-            #print(f'Ended download link {download_link} {datetime.datetime.now()}')
 
     
-async def main(websocket, path, listener, message_queue):
+async def main(websocket, path):
+
+        message_queue = asyncio.Queue()
+        test_queue = asyncio.Queue()
+
+        listener = AsyncWebsocketListener(test_queue)
         
         request = await websocket.recv()
+        await websocket.send('200')
         request_json = json.loads(request)
         data = request_json.get('data')
         if data:
@@ -195,21 +310,28 @@ async def main(websocket, path, listener, message_queue):
             if data.get('type') == 'voice':
                 await listener.voice_producer(data.get('user_id'), data.get('file_id'))
             if data.get('type') == 'message':
+                print('Message')
                 await listener.redis_list_returner(data.get('user_id'))
+            if data.get('type') == 'test':
+                print('Test message')
+                print(test_queue)
+                await test_queue.put(data.get('message'))
 
         else:
             pass
 
-        await websocket.send('200')
-
 
 if __name__ == '__main__':
 
-    message_queue = asyncio.Queue()
+    if DEBUG:
+        host = 'localhost'
+        port = 8765
+    else:
+        host = '0.0.0.0'
+        port = os.environ.get('PORT')
 
     listener = AsyncTelegramListener()
-    start_server = websockets.serve(functools.partial(main, listener=listener, message_queue = message_queue), "0.0.0.0", os.environ.get('PORT'))
-    
+
+    start_server = websockets.serve(main, host = host, port = port)
     asyncio.get_event_loop().run_until_complete(asyncio.gather(listener.redis_connection_listener(5), start_server))
     asyncio.get_event_loop().run_forever()
-
